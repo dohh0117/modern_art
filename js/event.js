56a82825e94cb860bf9ff07fe36b02a51499aacd
dohh0117 <126572290+dohh0117@users.noreply.github.com>
2023-11-01 17:21:20 +0900

    Add files via upload

--- js/event.js
+++ js/event.js
@@ -18,7 +18,24 @@ window.onload =function(){
    //    document.getElementById('header').classList.add('on')
    // })
  
+   let currentIndex =0;
+   const sliderWrap= document.querySelector("#slider > ul");
+   const slider =document.querySelectorAll(".slide");
+   const sliderClone =sliderWrap.firstElementChild.cloneNode(true);
+   sliderWrap.appendChild(sliderClone);
 
+   setInterval(()=>{
+      currentIndex++;
+      sliderWrap.style.marginLeft =-currentIndex*1380+"px";
+      sliderWrap.style.transition ="all 0.6s";
+      if(currentIndex ==3){
+         setTimeout(()=>{
+            sliderWrap.style.transition="0s";
+            sliderWrap.style.marginLeft="0";
+            currentIndex=0;
+         },700)
+      }
+   },5000)
  
 
    
